1eca52e57efe4624958d0ab8b14a0c3e89433baf
coco <kokotenrico@gmail.com>
2022-06-05 09:52:02 +0200

    Implement playlists and coins, refactor responses

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ const users = [
     name: "Dino Merlić",
     playlists: ["rock", "rap"],
     duels: [],
-    medals: ["won 10 games"],
+    coins: 43,
+    // medals: ["won 10 games"],
     "games played": 12,
     "games won": 7,
     "games lost": 3,
@@ -24,7 +25,8 @@ const users = [
     name: "Senka Miletić",
     playlists: ["rock", "trap"],
     duels: [{ against: 2, playlist: "rock", played: 1, score: 75 }],
-    medals: ["won with a perfect score"],
+    coins: 10,
+    // medals: ["won with a perfect score"],
     "games played": 6,
     "games won": 2,
     "games lost": 4,
@@ -35,7 +37,8 @@ const users = [
     name: "Fedja Sakić",
     playlists: ["folk", "rap"],
     duels: [{ against: 1, playlist: "rock", played: 0, score: 0 }],
-    medals: [],
+    coins: 3,
+    // medals: [],
     "games played": 3,
     "games won": 0,
     "games lost": 3,
@@ -77,6 +80,30 @@ const songsAudio = new Map([
   [3, "Previse_suza_u_mom_pivu.mp3"],
 ]);
 
+const playlists = [
+  {
+    id: 0,
+    title: "rock",
+    details: "All the YU and post-YU rock your heart desires!",
+    price: 25,
+    songs: [0, 1, 2, 3],
+  },
+  {
+    id: 1,
+    title: "folk",
+    details: "For the animal in you here is the best glass-shattering music!",
+    price: 25,
+    songs: [4, 5, 6, 7],
+  },
+  {
+    id: 2,
+    title: "rap",
+    details: "Music from the streets for the streets!",
+    price: 25,
+    songs: [8, 9, 10, 11],
+  },
+];
+
 app.get("/", (req, res) => {
   res.status(200);
   res.send("Home");
@@ -113,6 +140,18 @@ app.get("/song/:id/audio", (req, res) => {
   res.send(specificSongAudio);
 });
 
+app.get("/playlist", (req, res) => {
+  res.status(200);
+  res.send(playlists);
+});
+
+app.get("/playlist/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const specificPlaylist = playlists.find((playlist) => playlist.id === id);
+  res.status(200);
+  res.send(specificPlaylist);
+});
+
 // slanje izazova drugom playeru
 app.put("/duel", (req, res) => {
   const { body } = req;
@@ -133,7 +172,7 @@ app.put("/duel", (req, res) => {
     users[playerTwoIndex].duels.find((duel) => duel.against == playerOneId)
   ) {
     res.status(200);
-    res.send("Duel already active");
+    res.send({ requestCompleted: false, reason: "Duel already active" });
   } else {
     users[playerOneIndex].duels.push({
       against: playerTwoId,
@@ -153,10 +192,7 @@ app.put("/duel", (req, res) => {
     });
 
     res.status(200);
-    res.send({
-      playerOne: users[playerOneIndex],
-      playerTwo: users[playerTwoIndex],
-    });
+    res.send({ requestCompleted: true });
   }
 });
 
@@ -170,37 +206,43 @@ app.put("/duel/end", (req, res) => {
   if (playerOneScore > playerTwoScore) {
     users[playerOneIndex]["games played"] += 1;
     users[playerOneIndex]["games won"] += 1;
+    users[playerOneIndex].coins += 3;
     users[playerOneIndex].duels = users[playerOneIndex].duels.filter(
       (duel) => duel.against == playerTwoId
     );
     users[playerTwoIndex]["games played"] += 1;
     users[playerTwoIndex]["games lost"] += 1;
+    users[playerTwoIndex].coins += 1;
     users[playerTwoIndex].duels = users[playerTwoIndex].duels.filter(
       (duel) => duel.against == playerOneId
     );
     res.status(200);
-    res.send("Player one won!");
+    res.send({ winner: playerOneId });
   } else if (playerOneScore < playerTwoScore) {
     users[playerTwoIndex]["games played"] += 1;
     users[playerTwoIndex]["games won"] += 1;
+    users[playerTwoIndex].coins += 3;
     users[playerTwoIndex].duels = users[playerTwoIndex].duels.filter(
       (duel) => duel.against == playerOneId
     );
     users[playerOneIndex]["games played"] += 1;
     users[playerOneIndex]["games lost"] += 1;
+    users[playerOneIndex].coins += 1;
     users[playerOneIndex].duels = users[playerOneIndex].duels.filter(
       (duel) => duel.against == playerTwoId
     );
     res.status(200);
-    res.send("Player two won!");
+    res.send({ winner: playerTwoId });
   } else {
     users[playerOneIndex]["games played"] += 1;
     users[playerOneIndex]["games tied"] += 1;
+    users[playerOneIndex].coins += 2;
     users[playerOneIndex].duels = users[playerOneIndex].duels.filter(
       (duel) => duel.against == playerTwoId
     );
     users[playerTwoIndex]["games played"] += 1;
     users[playerTwoIndex]["games tied"] += 1;
+    users[playerTwoIndex].coins += 2;
     users[playerTwoIndex].duels = users[playerTwoIndex].duels.filter(
       (duel) => duel.against == playerOneId
     );
@@ -225,10 +267,27 @@ app.delete("/duel", (req, res) => {
   );
 
   res.status(200);
-  res.send({
-    playerOne: users[playerOneIndex],
-    playerTwo: users[playerTwoIndex],
-  });
+  res.send("OK");
+});
+
+app.put("/shop/playlist", (req, res) => {
+  const { body } = req;
+  const { playerId, playlistTitle } = body;
+
+  const playerIndex = users.findIndex((user) => user.id == playerId);
+  const playlistIndex = playlists.findIndex(
+    (playlist) => playlist.title == playlistTitle
+  );
+
+  if (users[playerIndex].coins > playlists[playlistIndex].price) {
+    users[playerIndex].coins -= playlists[playlistIndex].price;
+    users[playerIndex].playlists.push(playlists[playlistIndex].title);
+    res.status(200);
+    res.send({ transactionCompleted: true });
+  } else {
+    res.status(200);
+    res.send({ transactionCompleted: false, reason: "Not enough funds" });
+  }
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
