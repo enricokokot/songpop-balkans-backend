33fb66442fca6f841963c2c3b01caab7d513f1e1
coco <kokotenrico@gmail.com>
2022-06-05 13:15:08 +0200

    Add songs

--- src/songsDetails.js
+++ src/songsDetails.js
@@ -23,4 +23,52 @@ export const songsDetails = [
     artist: "Prljavo kazalište",
     playlist: "rock",
   },
+  {
+    id: 4,
+    title: "Aspirin",
+    artist: "Seka",
+    playlist: "folk",
+  },
+  {
+    id: 5,
+    title: "Mešaj, mala (feat. Saša Matić)",
+    artist: "Rada Manojlović",
+    playlist: "folk",
+  },
+  {
+    id: 6,
+    title: "Šampanjac",
+    artist: "Mile Kitić",
+    playlist: "folk",
+  },
+  {
+    id: 7,
+    title: "Trezan",
+    artist: "Sinan Sakić",
+    playlist: "folk",
+  },
+  {
+    id: 8,
+    title: "No sikiriki no",
+    artist: "Edo Maajka",
+    playlist: "rap",
+  },
+  {
+    id: 9,
+    title: "Ne može",
+    artist: "Vojko V",
+    playlist: "rap",
+  },
+  {
+    id: 10,
+    title: "Esi mi dobar",
+    artist: "Bad Copy",
+    playlist: "rap",
+  },
+  {
+    id: 11,
+    title: "Malo san maka",
+    artist: "The Beat Fleet",
+    playlist: "rap",
+  },
 ];
