b9490cb2280022e36c326da5bf3a65da2b37bacf
coco <kokotenrico@gmail.com>
2022-07-06 19:36:59 +0200

    Reduce multiple resources into one

--- src/routes/Playlist.js
+++ src/routes/Playlist.js
@@ -17,3 +17,167 @@ router.get("/:id", async (req, res) => {
   res.status(200);
   res.send(specificPlaylist);
 });
+
+router.put("/buy", async (req, res) => {
+  const { playerId, playlistTitle } = req.body;
+
+  let db = await connect();
+  let playerIndex = await db.collection("users").findOne({ id: playerId });
+  const thePlaylist = await db
+    .collection("playlists")
+    .findOne({ title: playlistTitle });
+
+  if (playerIndex.coins > thePlaylist.price) {
+    let playlistPrice = thePlaylist.price;
+    let playlistTitle = thePlaylist.title;
+    let buy1 = await db
+      .collection("users")
+      .updateOne({ id: playerId }, { $inc: { coins: -playlistPrice } });
+    let buy2 = await db
+      .collection("users")
+      .updateOne({ id: playerId }, { $push: { playlists: playlistTitle } });
+    res.status(200);
+    res.send({ transactionCompleted: true });
+  } else {
+    res.status(200);
+    res.send({ transactionCompleted: false, reason: "Not enough funds" });
+  }
+});
+
+router.get("/:id/game", async (req, res) => {
+  const playlistId = req.params.id;
+  let db = await connect();
+  let thePlaylist = await db
+    .collection("playlists")
+    .findOne({ title: playlistId });
+  const playlistSongsMixed = thePlaylist.songs;
+
+  const roundTypes = ["artist", "title"];
+  const firstRoundType =
+    roundTypes[Math.floor(Math.random() * roundTypes.length)];
+  const secondRoundType =
+    roundTypes[Math.floor(Math.random() * roundTypes.length)];
+  const thirdRoundType =
+    roundTypes[Math.floor(Math.random() * roundTypes.length)];
+
+  /***********************DANGER ZONE BELOW, NEED HELP***********************/
+  let shuffled;
+  let shuffledFiltered;
+
+  let _firstRoundSongs;
+  let _secondRoundSongs;
+  let _thirdRoundSongs;
+
+  let _firstRoundSongsString;
+  let _secondRoundSongsString;
+  let _thirdRoundSongsString;
+
+  do {
+    shuffled = [...playlistSongsMixed].sort(() => 0.5 - Math.random());
+    shuffledFiltered = shuffled.slice(0, 12);
+    _firstRoundSongs = shuffledFiltered.slice(0, 4);
+    _secondRoundSongs = shuffledFiltered.slice(4, 8);
+    _thirdRoundSongs = shuffledFiltered.slice(8, 12);
+
+    _firstRoundSongsString = await Promise.all(
+      _firstRoundSongs.map(async (songId) => {
+        if (firstRoundType === "title") {
+          let theSong = await db.collection("songs").findOne({ id: songId });
+          return theSong.title;
+        } else {
+          let theSong = await db.collection("songs").findOne({ id: songId });
+          return theSong.artist;
+        }
+      })
+    );
+
+    _secondRoundSongsString = await Promise.all(
+      _secondRoundSongs.map(async (songId) => {
+        if (secondRoundType === "title") {
+          let theSong = await db.collection("songs").findOne({ id: songId });
+          return theSong.title;
+        } else {
+          let theSong = await db.collection("songs").findOne({ id: songId });
+          return theSong.artist;
+        }
+      })
+    );
+
+    _thirdRoundSongsString = await Promise.all(
+      _thirdRoundSongs.map(async (songId) => {
+        if (thirdRoundType === "title") {
+          let theSong = await db.collection("songs").findOne({ id: songId });
+          return theSong.title;
+        } else {
+          let theSong = await db.collection("songs").findOne({ id: songId });
+          return theSong.artist;
+        }
+      })
+    );
+  } while (
+    new Set(_firstRoundSongsString).size !== _firstRoundSongsString.length ||
+    new Set(_secondRoundSongsString).size !== _secondRoundSongsString.length ||
+    new Set(_thirdRoundSongsString).size !== _thirdRoundSongsString.length
+  );
+
+  const firstRoundSongs = _firstRoundSongs;
+  const secondRoundSongs = _secondRoundSongs;
+  const thirdRoundSongs = _thirdRoundSongs;
+
+  const firstRoundSongsString = _firstRoundSongsString;
+  const secondRoundSongsString = _secondRoundSongsString;
+  const thirdRoundSongsString = _thirdRoundSongsString;
+
+  /**************************DANGER ZONE ABOVE, NEED HELP**************************/
+
+  const firstRoundCorrectAnswer =
+    firstRoundSongs[Math.floor(Math.random() * firstRoundSongs.length)];
+  const secondRoundCorrectAnswer =
+    secondRoundSongs[Math.floor(Math.random() * secondRoundSongs.length)];
+  const thirdRoundCorrectAnswer =
+    thirdRoundSongs[Math.floor(Math.random() * thirdRoundSongs.length)];
+
+  const firstRoundCorrectAnswerObject = await db
+    .collection("songs")
+    .findOne({ id: firstRoundCorrectAnswer });
+  const secondRoundCorrectAnswerObject = await db
+    .collection("songs")
+    .findOne({ id: secondRoundCorrectAnswer });
+  const thirdRoundCorrectAnswerObject = await db
+    .collection("songs")
+    .findOne({ id: thirdRoundCorrectAnswer });
+
+  const firstRoundCorrectAnswerString = await firstRoundCorrectAnswerObject[
+    firstRoundType
+  ];
+  const secondRoundCorrectAnswerString = await secondRoundCorrectAnswerObject[
+    secondRoundType
+  ];
+  const thirdRoundCorrectAnswerString = await thirdRoundCorrectAnswerObject[
+    thirdRoundType
+  ];
+
+  const roundsData = {
+    0: {
+      songs: firstRoundSongsString,
+      correctAnswer: firstRoundCorrectAnswerString,
+      correctAnswerId: firstRoundCorrectAnswer,
+      playerPointsEarned: 0,
+    },
+    1: {
+      songs: secondRoundSongsString,
+      correctAnswer: secondRoundCorrectAnswerString,
+      correctAnswerId: secondRoundCorrectAnswer,
+      playerPointsEarned: 0,
+    },
+    2: {
+      songs: thirdRoundSongsString,
+      correctAnswer: thirdRoundCorrectAnswerString,
+      correctAnswerId: thirdRoundCorrectAnswer,
+      playerPointsEarned: 0,
+    },
+  };
+
+  res.status(200);
+  res.send({ transactionCompleted: true, roundsData, playlistId });
+});
