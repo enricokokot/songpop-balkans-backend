29f01f5324c32d669fc9c9ec80e73381dbb4d819
coco <kokotenrico@gmail.com>
2022-07-09 18:22:09 +0200

    Comment out module missing from remote

--- src/routes/Song.js
+++ src/routes/Song.js
@@ -2,27 +2,27 @@ import express from "express";
 import connect from "../db.js";
 import fs from "fs";
 import assert from "assert";
-import { songs } from "../songs.js";
+// import { songs } from "../songs.js";
 import { GridFSBucket } from "mongodb";
 import { resolve } from "path";
 
 export const router = express.Router();
 
 // upload pjesama sa lokalnog storage-a na mongo
-router.post("/", async (req, res) => {
-  let db = await connect();
-  songs.forEach((song) => {
-    let songUploadResult = db.collection("songs").insertOne({
-      id: song.id,
-      title: song.title,
-      artist: song.artist,
-      playlist: song.playlist,
-      file: song.file.slice(73),
-    });
-  });
-  res.status(200);
-  res.send("Songs uploaded successfully!");
-});
+// router.post("/", async (req, res) => {
+//   let db = await connect();
+//   songs.forEach((song) => {
+//     let songUploadResult = db.collection("songs").insertOne({
+//       id: song.id,
+//       title: song.title,
+//       artist: song.artist,
+//       playlist: song.playlist,
+//       file: song.file.slice(73),
+//     });
+//   });
+//   res.status(200);
+//   res.send("Songs uploaded successfully!");
+// });
 
 router.get("/", async (req, res) => {
   let db = await connect();
@@ -32,20 +32,20 @@ router.get("/", async (req, res) => {
 });
 
 // upload specifične pjesme sa lokalnog storage-a na mongo
-router.post("/:id", async (req, res) => {
-  const id = Number(req.params.id);
-  let db = await connect();
-  const songToBeUploaded = songs.find((song) => song.id === id);
-  let songUploadResult = await db.collection("songs").insertOne({
-    id: songToBeUploaded.id,
-    title: songToBeUploaded.title,
-    artist: songToBeUploaded.artist,
-    playlist: songToBeUploaded.playlist,
-    file: songToBeUploaded.file.slice(73),
-  });
-  res.status(200);
-  res.send("Songs uploaded successfully!");
-});
+// router.post("/:id", async (req, res) => {
+//   const id = Number(req.params.id);
+//   let db = await connect();
+//   const songToBeUploaded = songs.find((song) => song.id === id);
+//   let songUploadResult = await db.collection("songs").insertOne({
+//     id: songToBeUploaded.id,
+//     title: songToBeUploaded.title,
+//     artist: songToBeUploaded.artist,
+//     playlist: songToBeUploaded.playlist,
+//     file: songToBeUploaded.file.slice(73),
+//   });
+//   res.status(200);
+//   res.send("Songs uploaded successfully!");
+// });
 
 router.get("/:id", async (req, res) => {
   const id = Number(req.params.id);
@@ -80,26 +80,26 @@ router.get("/:id/audio", async (req, res) => {
 });
 
 // upload audio file-a sa lokalne mašine na mongo
-router.post("/:id/audio", async (req, res) => {
-  const id = Number(req.params.id);
-  if (songs.find((song) => song.id === id)) {
-    const song = songs.find((song) => song.id === id);
-    const songAudioLocation = song.file;
-    const songAudioName = songAudioLocation.slice(73);
-    let db = await connect();
-    let bucket = new GridFSBucket(db);
-    fs.createReadStream(songAudioLocation)
-      .pipe(bucket.openUploadStream(songAudioName))
-      .on("error", function (error) {
-        assert.ifError(error);
-      })
-      .on("finish", function () {
-        console.log("File uploaded!");
-      });
-    res.status(200);
-    res.send(songAudioName);
-  } else {
-    res.status(200);
-    res.send("No song with that id");
-  }
-});
+// router.post("/:id/audio", async (req, res) => {
+//   const id = Number(req.params.id);
+//   if (songs.find((song) => song.id === id)) {
+//     const song = songs.find((song) => song.id === id);
+//     const songAudioLocation = song.file;
+//     const songAudioName = songAudioLocation.slice(73);
+//     let db = await connect();
+//     let bucket = new GridFSBucket(db);
+//     fs.createReadStream(songAudioLocation)
+//       .pipe(bucket.openUploadStream(songAudioName))
+//       .on("error", function (error) {
+//         assert.ifError(error);
+//       })
+//       .on("finish", function () {
+//         console.log("File uploaded!");
+//       });
+//     res.status(200);
+//     res.send(songAudioName);
+//   } else {
+//     res.status(200);
+//     res.send("No song with that id");
+//   }
+// });
